2f59d363cea8f81b17b18a584907caa0247d6694
Rogix <12768403+rogix@users.noreply.github.com>
2024-02-20 23:43:19 -0300

    remove post coverage

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -6,7 +6,6 @@
 
 	const post = data.props?.post.publication?.post?.content.html;
 	const postTitle = data.props?.post.publication?.post?.title;
-	const postCoverImage = data.props?.post.publication?.post?.coverImage?.url;
 	const postTags = data.props?.post.publication?.post?.tags;
 </script>
 
@@ -15,7 +14,6 @@
 <main class="mx-auto my-10 max-w-5xl px-6">
 	<article class="prose mx-auto dark:prose-invert">
 		<h1 class="font-sans font-semibold">{postTitle}</h1>
-		<img src={postCoverImage} alt="Post" class="h-[400px] w-full rounded-2xl object-cover" />
 		{@html post}
 		<div class="flex gap-2">
 			{#if postTags}
